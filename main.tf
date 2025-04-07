394a988531592210fd7621a7cc6c24fb3601bd2b
Gaukhar Shegirova Ergin <65710629+gaukharshegirova@users.noreply.github.com>
2025-04-07 17:01:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t3a.medium"
-  key_name = "firstpem"
+  key_name = "xxxxx"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   user_data =  <<-EOF
           #! /bin/bash
